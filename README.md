ced720d61e1918800f8282234fbc2d45e194c648
Szymon <szymonpakula98@gmail.com>
2019-11-20 14:48:51 +0100

    Starting files. TODO: Test file loader mp3, SCSS

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ForeCCast
 CodersCamp2019 Projekt 3 
+Aplikacja pogodowa
+
+## Uruchomienie:
+1. Sklonować repo na dysk i otworzyć w konsoli
+2. npm install
+3. npm start (projekt uruchomi się na localhost:8080)
+
+## Przydatne komendy
+
+npm run format - formatuje kod
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname),
+    filename: 'index.js',
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        loaders: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+};
