9abab6d705a203ba29c266c6ef64cbf430fd3948
Olek <aleksander.atamanczuk@gmail.com>
2019-11-22 18:41:03 +0100

    The getSusnsetSunrise function has been moved to a separate file

--- src/main.js
+++ src/main.js
@@ -1,33 +1,13 @@
 import './style.scss';
+import * as sunTime from './sunsetSunriseTime';
 
-console.log('main.js ready to serve');
+let country = 'pl';
+let city = 'Walbrzych';
 
 // Show info when the user clicks the button
-document.getElementById('getText').addEventListener('click', getSunsetSunrise);
+document.getElementById('getText')
+.addEventListener('click', () => {
+    sunTime.getSunsetSunrise(country, city); 
+});
 
-let City = 'Sosnowiec';
-let Country = 'pl';
-let KeyAPI = 'a5c8e964ba083b95272f66d4becc65b2' 
-let Link = 'http://api.openweathermap.org/data/2.5/weather?q='+City+','+Country+'&APPID='+KeyAPI;
-
-// Take and display info from API (https://openweathermap.org/)
-function getSunsetSunrise() {
-  fetch(Link)
-    .then(res => res.json())
-    .then(data => {
-      // Convert epoch time to human time
-      let Sunrise = new Date(data.sys.sunrise * 1000);
-      let Sunset = new Date(data.sys.sunset * 1000);
-      let timeSunrise = Sunrise.toLocaleTimeString();
-      let timeSunset = Sunset.toLocaleTimeString();
-
-      let output = `<h2>${data.name}</h2>`;     // Display City
-      output += `
-            <div>
-              <p>Sunrise: ${timeSunrise}</p>
-              <p>Sunset: ${timeSunset}</p>
-            </div>
-          `;
-      document.getElementById('output').innerHTML = output;
-    });
-}
+console.log('main.js ready to serve');
