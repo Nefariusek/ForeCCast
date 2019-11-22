0d750268ee9e9be75f896a0dc90ab5d74c93e90a
Mateusz <mateusz.fordas@gmail.com>
2019-11-22 19:39:44 +0100

    Current weather data

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './style.scss';
 import * as news from './news';
 import * as sunTime from './sunsetSunriseTime';
+import Weather from './weather';
 
 let country = 'pl';
 let language = 'pl';
@@ -15,5 +16,15 @@ document.getElementById('getText')
 .addEventListener('click', () => {
     sunTime.getSunsetSunrise(country, city); 
 });
+
+// Default current weather data
+let weather = new Weather(51.107883, 17.038538);
+weather.apiCall(weather.setURL());
+//Current weather data for coordinates from user
+
+document.getElementById('getCoordinates').addEventListener('click', () => {
+    weather.getCoordinates();
+});
+
 
 console.log('main.js ready to serve');
